59f91d1744c4fdbd97b25326a30794f94960898c
Sourav <souravraj664@gmail.com>
2025-08-25 12:08:37 +0530

    added blogs and resume

---
+++ assets/js/swiper.js
@@ -0,0 +1,35 @@
+new Swiper('.card-wrapper', {
+  // Optional parameters
+
+  loop: true,
+  spaceBetween: 30,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
+
+  // And if we need scrollbar
+  
+});
